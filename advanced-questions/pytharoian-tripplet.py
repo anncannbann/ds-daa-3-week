dc16b1babd050d1058ef3eb766a6f1f820038dae
anncannbann <anncannbann@gmail.com>
2021-11-26 01:12:04 +0530

    Binary Tree

--- advanced-questions/pytharoian-tripplet.py
+++ advanced-questions/pytharoian-tripplet.py
@@ -1,3 +1,4 @@
+
 # Given an array of integers, write a function that returns true if there is a triplet (a, b, c) that satisfies 
 # a2 + b2 = c2.
 
@@ -27,3 +28,36 @@ arr = [3, 1, 4, 6, 5]
 print(triplet(arr))
 
 
+# # Python program that returns true if there is
+# # a Pythagorean Triplet in a given array.
+ 
+# # Returns true if there is Pythagorean
+# # triplet in ar[0..n-1]
+# def isTriplet(ar, n):
+#     # Square all the elements
+#     for i in range(n):
+#         ar[i] = ar[i] * ar[i]
+ 
+#     # sort array elements
+#     ar.sort()
+ 
+#     # fix one element
+#     # and find other two
+#     # i goes from n - 1 to 2
+#     for i in range(n-1, 1, -1):
+#         # start two index variables from
+#         # two corners of the array and
+#         # move them toward each other
+#         j = 0
+#         k = i - 1
+#         while (j < k):
+#             # A triplet found
+#             if (ar[j] + ar[k] == ar[i]):
+#                 return True
+#             else:
+#                 if (ar[j] + ar[k] < ar[i]):
+#                     j = j + 1
+#                 else:
+#                     k = k - 1
+#     # If we reach here, then no triplet found
+#     return False
--- ds/binary trees/1.py
+++ ds/binary trees/1.py
@@ -1,5 +1,63 @@
+
 class Node:
     def __init__(self,key):
         self.left = None
         self.right = None
         self.val = key
+
+'''
+Algorithm Inorder(tree)
+   1. Traverse the left subtree, i.e., call Inorder(left-subtree)
+   2. Visit the root.
+   3. Traverse the right subtree, i.e., call Inorder(right-subtree)
+Uses of Inorder '''
+
+def PrintInorder(root):
+    if root:
+        PrintInorder(root.left)
+        print(root.val)
+        print(root.right)
+
+
+
+'''
+Algorithm Preorder(tree)
+   1. Visit the root.
+   2. Traverse the left subtree, i.e., call Preorder(left-subtree)
+   3. Traverse the right subtree, i.e., call Preorder(right-subtree) 
+
+
+'''
+
+def PrintPreorder(root):
+    if root:
+        print(root.val)
+        print(root.left)
+        print(root.right)
+
+'''
+Algorithm Postorder(tree)
+   1. Traverse the left subtree, i.e., call Postorder(left-subtree)
+   2. Traverse the right subtree, i.e., call Postorder(right-subtree)
+   3. Visit the root.
+
+'''
+
+def PrintPostorder(root):
+    if root:
+        print(root.left)
+        print(root.right)
+        print(root.val)
+
+
+
+
+
+
+
+root = Node(1)
+root.left      = Node(2)
+root.right     = Node(3)
+root.left.left = Node(4)
+
+PrintPostorder(root)
