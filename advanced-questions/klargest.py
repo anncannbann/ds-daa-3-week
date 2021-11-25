957e4469ce6305172466eac08cd0df72f04aee11
anncannbann <anncannbann@gmail.com>
2021-11-25 23:46:29 +0530

    k largest

---
+++ advanced-questions/klargest.py
@@ -0,0 +1,12 @@
+#k largest(or smallest) elements in an array | added Min Heap method
+#For example, if the given array is [1, 23, 12, 9, 30, 2, 50] and you are asked for the largest 3 elements i.e., k = 3 then your program should print 50, 30, and 23.
+
+
+
+arr =[1, 23, 12, 9, 30, 2, 50]
+k =3
+arr.sort(reverse = True)
+
+print(arr)
+for i in range(k):
+    print(arr[i], end=" ")
